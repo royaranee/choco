4663505a7fdc0e40809f69572c94da7090fff0da
royaranee <royaranee@gmail.com>
2018-04-16 12:07:36 +0530

    Commit message

--- login.php
+++ login.php
@@ -4,6 +4,7 @@
 			<form method="POST" action="profile.php">
 			Username: <input type="text" name="username" required><br />
 			Password: <input type="password" name="password" required><br />
+			Branch: <input type="text" name="branch" required><br />
 			<input type="submit" value="Login">
 			</form>
 		</body>
